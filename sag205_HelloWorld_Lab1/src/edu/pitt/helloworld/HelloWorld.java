318a53f1a4d6eb99039060b5830e1103baa17516
gandhisaajan22 <112505014+gandhisaajan22@users.noreply.github.com>
2022-09-05 15:25:46 -0400

    Lab 1

---
+++ sag205_HelloWorld_Lab1/src/edu/pitt/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package edu.pitt.helloworld;
+
+public class HelloWorld {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD!");
+
+	}
+
+}
